1595619923926a0f768514831d695fef6c3c2fa4
Nelo <nelopauselli@gmail.com>
2022-03-07 10:22:40 -0300

    send and recibe message

--- adapters/jsonRpcAdapter.js
+++ adapters/jsonRpcAdapter.js
@@ -9,6 +9,35 @@ class JsonRpcAdapter {
     createAccount(name) {
         return new Account(name, this.provider);
     }
+
+    async getBlockNumber(){
+        return await this.provider.getBlockNumber();
+    }
+
+    async readMessages(address) {
+        let currentBlockNumber = await this.provider.getBlockNumber();
+
+        let messages = [];
+
+        let block = await this.provider.getBlock(currentBlockNumber);
+        for (let t = 0; t < block.transactions.length; t++) {
+            let transactionHash = block.transactions[t];
+            let transaction = await this.provider.getTransaction(transactionHash);
+            if (transaction.to == address) {
+                if (transaction.data) {
+                    let raw = transaction.data.slice(2);
+                    let message = {
+                        block: block.number,
+                        content: Buffer.from(raw, 'hex')
+                    };
+
+                    messages.push(message);
+                }
+            }
+        }
+
+        return messages;
+    }
 }
 
 module.exports = JsonRpcAdapter;
--- test/messages.test.js
+++ test/messages.test.js
@@ -21,4 +21,21 @@ describe("Messages", function () {
         assert(tx.data);
         assert.equal('0x48656c6c6f2063727970746f20776f726c6421', tx.data);
     });
+
+    it("Send and read plain message", async function () {
+        let lastBlockNumber = await adapter.getBlockNumber();
+
+        let buffer = encoder.encode('Hello crypto world!');
+        let tx = await alice.send(messagesOnChainPublicAddress, buffer);
+        await tx.wait();
+
+        let messages = await adapter.readMessages(messagesOnChainPublicAddress);
+        assert(messages);
+        assert(1, messages.length);
+
+        let message = messages[0];
+        let content = encoder.decode(message.content);
+        assert.equal('Hello crypto world!', content);
+        assert.equal(lastBlockNumber + 1, message.block);
+    });
 })
