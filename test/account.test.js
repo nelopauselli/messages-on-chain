e94dedacc13b7f67dae2002915c0e3908f68ec24
Nelo <nelopauselli@gmail.com>
2022-03-07 17:38:44 -0300

    Account doesn't exist

--- test/account.test.js
+++ test/account.test.js
@@ -3,7 +3,7 @@ var assert = require('assert');
 var Adapter = require('./../adapters/jsonRpcAdapter');
 
 describe("Accounts", function () {
-    before(function(){
+    before(function () {
         adapter = new Adapter('http://localhost:7545');
     });
 
@@ -13,6 +13,19 @@ describe("Accounts", function () {
         assert(alice);
         assert.equal('alice', alice.name);
         assert(alice.wallet);
-        assert(alice.wallet.publicKey);
+        assert.equal("0xCF50b5e4B3AFe99FBfB2d93DCb4BdFE670477505", alice.wallet.address);
+    });
+
+    it("Bob", function () {
+        let bob = adapter.createAccount('bob');
+
+        assert(bob);
+        assert.equal('bob', bob.name);
+        assert(bob.wallet);
+        assert.equal("0x5c19C5406426783BA02b1cf322F4332b79a8B564", bob.wallet.address);
+    });
+
+    it("Batman", function () {
+        assert.rejects(() => adapter.createAccount('batman'));
     });
 })
