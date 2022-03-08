6df19292ae1fd206ffe507be0886da1b40660ad9
Nelo <nelopauselli@gmail.com>
2022-03-08 16:58:15 -0300

    create wallet on startup

---
+++ index.js
@@ -0,0 +1,37 @@
+var Adapter = require('./adapters/jsonRpcAdapter');
+const fs = require('fs'),
+    path = require('path');
+
+const adapter = new Adapter('http://localhost:7545');
+
+async function main() {
+    console.log('connecting...');
+    let block = await adapter.getBlockNumber();
+    console.log(`block: ${block}`);
+
+    let dataPath = path.join(__dirname, '.data');
+    if (!fs.existsSync(dataPath)) fs.mkdirSync(dataPath);
+
+    let accountPath = path.join(dataPath, 'me');
+    if (!fs.existsSync(accountPath)) fs.mkdirSync(accountPath);
+
+    let privateKeyPath = path.join(accountPath, 'private.key');
+    let account;
+
+    if (!fs.existsSync(privateKeyPath)) {
+        account = await adapter.newAccount('me');
+        let wallet = account.wallet;
+        fs.writeFileSync(privateKeyPath, wallet.privateKey);
+        console.log('A new wallet was created');
+        console.log(`> Address: ${wallet.address}`);
+        console.log(`> Phrase: ${wallet.mnemonic.phrase}`);
+    }
+    else {
+        account = await adapter.createAccount('me');
+        let wallet = account.wallet;
+        console.log('Wallet was loaded');
+        console.log(`> Address: ${wallet.address}`);
+    }
+}
+
+main();
