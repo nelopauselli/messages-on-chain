f0066e9720ad83dbe59a0fa3cbdad5444cd495dc
Nelo <nelopauselli@gmail.com>
2022-03-09 06:58:21 -0300

    cleanup

--- settings.js
+++ settings.js
@@ -1,7 +1,6 @@
 class Settings {
     constructor() {
         this.messagesOnChainPublicAddress = '0x6d657373616765732d6f6e2d636861696e2d3031';
-        //this.messagesOnChainPublicAddress = '0x1111111111111111111111111111111111111111';
     }
 }
 
